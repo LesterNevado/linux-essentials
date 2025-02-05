8419da5239294c7010c0a97fd92c9669ebd4a429
LesterNevado <152840417+LesterNevado@users.noreply.github.com>
2025-02-05 01:42:38 +0000

    Activity 1

---
+++ activity1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# ACTIVITY 1
+
+# Ask for user input
+echo "ENTER NAME:"
+read NAME
+echo "ENTER AGE:"
+read AGE
+echo "ENTER PROGRAM:"
+read PROGRAM
+
+# Display a presonalized message
+echo "Hello $NAME, This is your age $AGE & your program is $PROGRAM"
